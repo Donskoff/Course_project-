0bfe7d125bd21a9b201fc328dec3a30d75162dc9
Андрей Донсков <bionet1971@gmail.com>
2025-07-28 21:32:51 +0300

    config fixes

--- config.py
+++ config.py
@@ -14,12 +14,3 @@ PATH_TO_GREETING = PATH / "src" / "utils.py"
 
 # Получение API ключа из переменных окружения
 API_KEY = os.getenv("API_KEY")
-
-# from pathlib import Path
-#
-#
-# PATH = Path(__file__).parent
-# PATH_TO_OPERATIONS = PATH / "data" / "operations.xlsx"
-# PATH_TO_USER_SETTINGS = PATH / "user_settings.json"
-# PATH_TO_LOGGER = PATH / "logs"
-# PATH_TO_GREETING = PATH / "src" / "utils.py"
